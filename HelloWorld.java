36bd797d4779b60c32c4d90234263a3f65d5dd15
Krishan Kant <Krishan.kantagarwal.che20@itbhu.ac.in>
2021-06-18 12:07:48 +0530

    changes in dev branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 System.out.println("HELLO WORLD");
 System.out.println("Short changes")
-
+System.out.println("Changes in dev")
 
  }
           }
